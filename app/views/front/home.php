354976c8f90db7de11d4094d6bf1fed2e917ad16
Ali Yara <ali.yara.fr@gmail.com>
2025-02-06 21:22:23 +0100

    initialisation

---
+++ app/views/front/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../../assets/css/output.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-4xl text-red-600 font-bold p-6 bg-gray-200 flex justify-center items-center h-screen">Ali Yara</h1>
+</body>
+</html>
