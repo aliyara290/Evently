ef35e9b0b4dae4cbe50aeb1f71abab888480c312
Ali Yara <ali.yara.fr@gmail.com>
2025-02-07 08:52:44 +0100

    Add AuthMiddleware class for prevent a none athorisation user to enter the pages

--- app/middleware/AuthMiddleware.php
+++ app/middleware/AuthMiddleware.php
@@ -2,5 +2,12 @@
 namespace App\Middleware;
 
 class AuthMiddleware {
-    
+    public function handle(array $allowRoles): bool {
+        session_start();
+        $role = $_SESSION["role"] ?? null;
+        if(!$role && !in_array($role, $allowRoles)) {
+            return false;
+        }
+        return true;
+    }
 }
