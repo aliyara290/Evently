ad35d601060c9985761acbfd0bf81775068cdbf5
Ali Yara <ali.yara.fr@gmail.com>
2025-02-07 08:53:48 +0100

    add more routes

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
 <?php 
+require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . "/../config/Database.php";
+use App\Core\Router;
+use App\Middleware\AuthMiddleware;
+use App\Controllers\HomeController;
+use App\Controllers\AppController;
+
+$router = new Router();
+
+$router->get("/", HomeController::class, "home");
+$router->get("/forbidden", AppController::class, "forbidden");
+$router->get("/404", AppController::class, "notFound");
+$router->dispatch();
