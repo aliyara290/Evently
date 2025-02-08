b41847910b3890b9d4b6e24c03037a8a38edc897
Ali Yara <ali.yara.fr@gmail.com>
2025-02-08 16:50:00 +0100

    add route for the Event page

--- public/index.php
+++ public/index.php
@@ -4,11 +4,13 @@ require __DIR__ . "/../config/Database.php";
 use App\Core\Router;
 use App\Middleware\AuthMiddleware;
 use App\Controllers\HomeController;
+use App\Controllers\EventController;
 use App\Controllers\AppController;
 
 $router = new Router();
 
 $router->get("/", HomeController::class, "home");
+$router->get("/event", EventController::class, "page");
 $router->get("/forbidden", AppController::class, "forbidden");
 $router->get("/404", AppController::class, "notFound");
 $router->dispatch();
