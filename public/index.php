a1b4f2486c7d6778d8a0bc72f85501c0b9ea6c39
Ali Yara <ali.yara.fr@gmail.com>
2025-02-08 23:48:12 +0100

    EP-59: create events page

--- public/index.php
+++ public/index.php
@@ -6,14 +6,16 @@ use App\Middleware\AuthMiddleware;
 use App\Controllers\HomeController;
 use App\Controllers\AuthController;
 use App\Controllers\EventController;
+use App\Controllers\EventsController;
 use App\Controllers\AppController;
 
 $router = new Router();
 
 $router->get("/", HomeController::class, "home");
 $router->get("/event", EventController::class, "page");
+$router->get("/events", EventsController::class, "page");
 $router->get("/forbidden", AppController::class, "forbidden");
+$router->get("/faqs", AppController::class, "faqs");
 $router->get("/404", AppController::class, "notFound");
-
 $router->get("/register", AuthController::class, "register");
 $router->dispatch();
