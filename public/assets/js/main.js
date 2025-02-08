bc397f3e30050a11af073b5f395a16b19c725f4e
ka-amina <aminakara400@gmail.com>
2025-02-08 07:35:25 +0100

    EP-46: Add Register Page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,19 @@
 let Evently;
+
+
+function togglePassword(id) {
+    const eye_slash = document.getElementById('eye-slash');
+    const eye = document.getElementById('eye');
+    const input = document.getElementById(id);
+    if (input.type === "password") {
+        input.type = "text";
+        eye_slash.classList.add('hidden');
+        eye.classList.remove('hidden');
+
+    } else {
+        input.type = "password";
+        eye_slash.classList.remove('hidden');
+        eye.classList.add('hidden');
+
+    }
+}
