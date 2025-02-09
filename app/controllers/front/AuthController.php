26d9d18ff9c775f66c803e6afed614803196da1a
Ali Yara <ali.yara.fr@gmail.com>
2025-02-09 22:39:58 +0100

    add profile/setting pages

--- app/controllers/front/AuthController.php
+++ app/controllers/front/AuthController.php
@@ -2,7 +2,8 @@
 namespace App\Controllers\Front;
 
 use App\Core\View;
-
+// client-id: 523106584100-hrtf77h34cd7hfsau27vislklbu7mfea.apps.googleusercontent.com
+// clinet-secret: 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
 class AuthController{
 
     public function register(){
--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use App\Core\Router;
 use App\Middleware\AuthMiddleware;
 use App\Controllers\Front\EventController;
 use App\Controllers\Front\EventsController;
+use App\Controllers\Front\SettingController;
+use App\Controllers\Front\UserProfileController;
 use App\Controllers\Front\HomeController;
 use App\Controllers\Front\OrganizerController;
 use App\controllers\Front\AuthController;
@@ -15,9 +17,10 @@ $router = new Router();
 $router->get("/", HomeController::class, "home");
 $router->get("/event", EventController::class, "page");
 $router->get("/events", EventsController::class, "page");
-$router->get("/forbidden", AppController::class, "forbidden");
+$router->get("/setting/profile", SettingController::class, "profile");
+$router->get("/setting/reset", SettingController::class, "setting");
+$router->get("/account/profile", UserProfileController::class, "page");
 $router->get("/faqs", AppController::class, "faqs");
-$router->get("/404", AppController::class, "notFound");
 $router->get("/register", AuthController::class, "register");
 $router->get("/organizer/dashboard", OrganizerController::class, "page");
 $router->get("/organizer/category", OrganizerController::class, "Category_page");
@@ -28,6 +31,7 @@ $router->get("/login", AuthController::class, "login");
 $router->get("/forgetPassword", AuthController::class, "forgetPassword");
 $router->get("/resetPassword", AuthController::class, "resetPassword");
 
-
+$router->get("/forbidden", AppController::class, "forbidden");
+$router->get("/404", AppController::class, "notFound");
 
 $router->dispatch();
