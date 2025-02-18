ee5de215bc50340f28e40d94f32204487c3f75d5
Ali Yara <ali.yara.fr@gmail.com>
2025-02-18 13:30:59 +0100

    add Default role in Session

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 require __DIR__ . "/../vendor/autoload.php";
 require __DIR__ . "/../config/Database.php";
+
 use App\Core\Router;
+use App\Core\Session;
 use App\Middleware\AuthMiddleware;
 use App\Controllers\Front\EventController;
 use App\Controllers\Front\EventsController;
@@ -24,7 +26,11 @@ use App\controllers\back\AppController as BackController;
 use App\Controllers\Mail\MailController;
 use App\Controllers\Front\PaymentController;
 
-
+if (!Session::has("user")) {
+    Session::set("user", [
+        "active_role" => "guest"
+    ]);
+}
 $router = new Router();
 
 $router->get("/", HomeController::class, "home");
@@ -39,7 +45,7 @@ $router->get("/setting/reset", SettingController::class, "setting", AuthMiddlewa
 $router->get("/account/profile", UserProfileController::class, "page", AuthMiddleware::class, ["Organizer", "Participant"]);
 $router->get("/faqs", AppController::class, "faqs");
 // $router->get("/organizer/dashboard", OrganizerController::class, "page");
-$router->get("/admin/manage-user", AdminController::class, "Mange_user_page", AuthMiddleware::class, ["Admin"]);
+$router->get("/organizer/manage-user", AdminController::class, "Mange_user_page", AuthMiddleware::class, ["Organizer"]);
 $router->get("/organizer/manage-events", OrganizerController::class, "Event_management_page", AuthMiddleware::class, ["Organizer"]);
 $router->get("/organizer/create", CreateEventController::class, "page", AuthMiddleware::class, ["Organizer"]);
 $router->get("/organizer/deleteEvent", OrganizerController::class, "deleteOrganizerEvent", AuthMiddleware::class, ["Organizer"]);
@@ -59,8 +65,8 @@ $router->post("/organizer/create", EventsController::class, "create", AuthMiddle
 //$router->get("/login", AuthController::class, "login");
 $router->get("/auth/google", AuthController::class, "googleLogin");
 $router->get("/auth/google-callback", AuthController::class, "googleCallback");
-$router->get("/forgetPassword", AuthController::class, "forgetPassword", AuthMiddleware::class, ["Geust"]);
-$router->get("/resetPassword", AuthController::class, "resetPassword", AuthMiddleware::class, ["Geust"]);
+$router->get("/forgetPassword", AuthController::class, "forgetPassword");
+$router->get("/resetPassword", AuthController::class, "resetPassword");
 // payments
 $router->post("/payment/stripe", PaymentController::class, "createStripePayment");  // Handle Stripe payment
 // $router->post("/payment/paypal", PaymentController::class, "createPayPalPayment");  // Handle PayPal payment
@@ -71,15 +77,15 @@ $router->get("/404", AppController::class, "notFound");
 $router->get("/payment/success", TicketController::class, "seccussPayment");
 $router->get("/payment/failed", AppController::class, "paymentFailed");
 // organizer router
-$router->post("/Category",CategoryController::class, "create", AuthMiddleware::class, ["Admin"]);
-$router->post("/Category/delete",CategoryController::class, "deleteCategory", AuthMiddleware::class, ["Admin"]);
-$router->post("/Category/update",CategoryController::class, "updateCategory", AuthMiddleware::class, ["Admin"]);
-$router->get("/admin/Mange_user",AdminControllerBack::class, "getallUsers", AuthMiddleware::class, ["Admin"]);
-$router->get("/active",AdminControllerBack::class, "UpduteStatustree");
+$router->post("/Category", CategoryController::class, "create", AuthMiddleware::class, ["Admin"]);
+$router->post("/Category/delete", CategoryController::class, "deleteCategory", AuthMiddleware::class, ["Admin"]);
+$router->post("/Category/update", CategoryController::class, "updateCategory", AuthMiddleware::class, ["Admin"]);
+$router->get("/admin/Mange_user", AdminControllerBack::class, "getallUsers", AuthMiddleware::class, ["Admin"]);
+$router->get("/active", AdminControllerBack::class, "UpduteStatustree");
 
 
 
-$router->get("/delete",AdminControllerBack::class, "deleteUser");
+$router->get("/delete", AdminControllerBack::class, "deleteUser");
 // Admin routers
 $router->get("/admin/dashboard", StasitickCountroler::class, "StatistickGlobale", AuthMiddleware::class, ["Admin"]);
 // $router->get("/admin/categories", BackController::class, "categories");
@@ -92,33 +98,35 @@ $router->get("/admin/users", BackController::class, "users", AuthMiddleware::cla
 $router->get("/admin/events", BackController::class, "events", AuthMiddleware::class, ["Admin"]);
 $router->get("/admin/users", AdminControllerBack::class, "getallUsers", AuthMiddleware::class, ["Admin"]);
 
-$router->get("/mange/delete",AdminControllerBack::class, "deleteUser", AuthMiddleware::class, ["Admin"]);
-$router->get("/mange/update",AdminControllerBack::class, "UpduteStatus", AuthMiddleware::class, ["Admin"]);
-$router->get("/mange/active",AdminControllerBack::class, "UpduteStatustree", AuthMiddleware::class, ["Admin"]);
+$router->get("/mange/delete", AdminControllerBack::class, "deleteUser", AuthMiddleware::class, ["Admin"]);
+$router->get("/mange/update", AdminControllerBack::class, "UpduteStatus", AuthMiddleware::class, ["Admin"]);
+$router->get("/mange/active", AdminControllerBack::class, "UpduteStatustree", AuthMiddleware::class, ["Admin"]);
 
 // mange event admin
-$router->get("/admin/events",AdminControllerBack::class,"getAllEvent", AuthMiddleware::class, ["Admin"]);
-$router->get("/admin/accept",AdminControllerBack::class,"updatestatus", AuthMiddleware::class, ["Admin"]);
-$router->get("/admin/refuse",AdminControllerBack::class,"updatestatusRefuse", AuthMiddleware::class, ["Admin"]);
-$router->get("/admin/delete",AdminControllerBack::class,"deleteEvent", AuthMiddleware::class, ["Admin"]);
-$router->post("/update-profile",SettingController::class,"updateProfile", AuthMiddleware::class, ["Participant", "Organizer"]);
+$router->get("/admin/events", AdminControllerBack::class, "getAllEvent", AuthMiddleware::class, ["Admin"]);
+$router->get("/admin/accept", AdminControllerBack::class, "updatestatus", AuthMiddleware::class, ["Admin"]);
+$router->get("/admin/refuse", AdminControllerBack::class, "updatestatusRefuse", AuthMiddleware::class, ["Admin"]);
+$router->get("/admin/delete", AdminControllerBack::class, "deleteEvent", AuthMiddleware::class, ["Admin"]);
+$router->post("/update-profile", SettingController::class, "updateProfile", AuthMiddleware::class, ["Participant", "Organizer"]);
 
 
 // $router->get("/organizer/createsponser", OrganizerController::class, "PageCreteSponser", AuthMiddleware::class, ["Organizer"]);
 
 
-$router->post("/Sponsoring",SponseurController::class, "create");
+$router->post("/Sponsoring", SponseurController::class, "create");
 
 $router->post("/switch-role", AuthController::class, "switchRole");
-$router->get("/event/teckte",EventController::class,"tecktepage");
+$router->get("/event/teckte", EventController::class, "tecktepage");
 
 
 
 
 $router->get("/organizer/sponser", SponseurController::class, "affichersponsorings", AuthMiddleware::class, ["Organizer"]);
 $router->get("/organizer/createsponser", OrganizerController::class, "PageCreteSponser", AuthMiddleware::class, ["Organizer"]);
-$router->post("/sponsor/delete",SponseurController::class, "deletesponsoring");
-$router->post("/sponsor/updatepage",OrganizerController::class, "updatesponsoringpage");
-$router->post("/Sponsoring/updte",SponseurController::class, "updatesponsoring");
+$router->post("/sponsor/delete", SponseurController::class, "deletesponsoring");
+$router->post("/sponsor/updatepage", OrganizerController::class, "updatesponsoringpage");
+$router->post("/Sponsoring/updte", SponseurController::class, "updatesponsoring");
+$router->get('/get-cities-by-region', CreateEventController::class, 'getCitiesByRegion');
 
-$router->dispatch();    
+
+$router->dispatch();
